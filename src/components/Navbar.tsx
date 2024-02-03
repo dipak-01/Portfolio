e509dd221300cbde2cab1f2435e93b84de2bba26
dipak-01 <imt_2022037@iiitm.ac.in>
2024-02-04 01:20:38 +0530

    navbar size fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,8 @@ export default function NavBar({
   const pathname = usePathname();
 
   return (
-    <nav aria-label="Main navigation">
-      <ul className="flex flex-col justify-between rounded-b-lg bg-slate-50 px-4 py-2 md:m-4 md:flex-row md:items-center md:rounded-xl">
+    <nav aria-label="Main navigation" className="m-auto md:w-9/12">
+      <ul className="flex flex-col justify-between rounded-b-lg   bg-slate-50 px-4 py-2 md:m-4 md:flex-row md:items-center md:rounded-xl">
         <div className="flex items-center justify-between">
           <NameLogo name={settings.data.name} />
           <button
@@ -115,7 +115,7 @@ function DesktopMenu({
   pathname: string;
 }) {
   return (
-    <div className="relative z-50 hidden flex-row items-center gap-1 bg-transparent py-0 md:flex">
+    <div className="relative z-50 hidden flex-row items-center gap-1 bg-transparent py-0 md:flex  ">
       {settings.data.navitems.map(({ link, label }, index) => (
         <React.Fragment key={label}>
           <li>
